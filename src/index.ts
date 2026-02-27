a5b9bf2c40417497161025cf52f704d48713d149
Abdellah2805 <abdullah.lghachi@gmail.com>
2026-02-27 10:17:25 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import pg from 'pg';
+const { Client} = pg;
+ 
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'library_db',
+    password: '123456789',
+    port: 5432,
+});
+
+async function testConnexion() {
+    try {
+        await client.connect();
+        console.log("Succès ! Le code est lié à PostgresSQL.");
+
+        const res = await client.query('SELECT * FROM "Livre"');
+        console.log("\n--- LIVRES DANS PGADMIN ---");
+        console.table(res.rows);
+    } catch (error) {
+        console.error("Erreur de connexion :", error);
+    } finally {
+        await client.end();
+    }
+}
+
+testConnexion();
+
+
